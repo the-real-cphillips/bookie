7dce93d7c85f8aeba5f543e6f2d2c44e806afc25
Craig Phillips <cphillips@2u.com>
2024-09-16 11:00:03 -0400

    feat: move `mock_response` into `utils/mock`

---
+++ utils/mock.py
@@ -0,0 +1,423 @@
+mock_response = [
+    {
+        "id": "82310eaf915bd9cb9c04c7053ba5a87e",
+        "sport_key": "americanfootball_ncaaf",
+        "sport_title": "NCAA Football",
+        "commence_time": "2024-09-02T18:11:00Z",
+        "home_team": "Milwaukee Brewers",
+        "away_team": "St. Louis Cardinals",
+        "bookmakers": [
+            {
+                "key": "fanduel",
+                "title": "FanDuel",
+                "last_update": "2024-09-01T19:42:12Z",
+                "markets": [
+                    {
+                        "key": "h2h",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {"name": "Milwaukee Brewers", "price": -162},
+                            {"name": "St. Louis Cardinals", "price": 136},
+                        ],
+                    },
+                    {
+                        "key": "spreads",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {
+                                "name": "Milwaukee Brewers",
+                                "price": 134,
+                                "point": -1.5,
+                            },
+                            {
+                                "name": "St. Louis Cardinals",
+                                "price": -162,
+                                "point": 1.5,
+                            },
+                        ],
+                    },
+                    {
+                        "key": "totals",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {"name": "Over", "price": -118, "point": 7.5},
+                            {"name": "Under", "price": -104, "point": 7.5},
+                        ],
+                    },
+                ],
+            }
+        ],
+    },
+    {
+        "id": "69315f03026308c181cf1d7b8f5b337d",
+        "sport_key": "baseball_mlb",
+        "sport_title": "MLB",
+        "commence_time": "2024-09-02T19:06:00Z",
+        "home_team": "Baltimore Orioles",
+        "away_team": "Chicago White Sox",
+        "bookmakers": [
+            {
+                "key": "fanduel",
+                "title": "FanDuel",
+                "last_update": "2024-09-01T19:42:12Z",
+                "markets": [
+                    {
+                        "key": "h2h",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {"name": "Baltimore Orioles", "price": -405},
+                            {"name": "Chicago White Sox", "price": 320},
+                        ],
+                    },
+                    {
+                        "key": "spreads",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {
+                                "name": "Baltimore Orioles",
+                                "price": -178,
+                                "point": -1.5,
+                            },
+                            {
+                                "name": "Chicago White Sox",
+                                "price": 146,
+                                "point": 1.5,
+                            },
+                        ],
+                    },
+                    {
+                        "key": "totals",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {"name": "Over", "price": -114, "point": 8.0},
+                            {"name": "Under", "price": -106, "point": 8.0},
+                        ],
+                    },
+                ],
+            }
+        ],
+    },
+    {
+        "id": "3ac935b4125d6bf7129a0203014bc81f",
+        "sport_key": "baseball_mlb",
+        "sport_title": "MLB",
+        "commence_time": "2024-09-02T20:11:00Z",
+        "home_team": "Arizona Diamondbacks",
+        "away_team": "Los Angeles Dodgers",
+        "bookmakers": [
+            {
+                "key": "fanduel",
+                "title": "FanDuel",
+                "last_update": "2024-09-01T19:42:12Z",
+                "markets": [
+                    {
+                        "key": "h2h",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {"name": "Arizona Diamondbacks", "price": 122},
+                            {"name": "Los Angeles Dodgers", "price": -144},
+                        ],
+                    },
+                    {
+                        "key": "spreads",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {
+                                "name": "Arizona Diamondbacks",
+                                "price": -134,
+                                "point": 1.5,
+                            },
+                            {
+                                "name": "Los Angeles Dodgers",
+                                "price": 112,
+                                "point": -1.5,
+                            },
+                        ],
+                    },
+                    {
+                        "key": "totals",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {"name": "Over", "price": -118, "point": 8.5},
+                            {"name": "Under", "price": -104, "point": 8.5},
+                        ],
+                    },
+                ],
+            }
+        ],
+    },
+    {
+        "id": "6118a904f8c7f96d07f8f2ad0bcfe6f3",
+        "sport_key": "baseball_mlb",
+        "sport_title": "MLB",
+        "commence_time": "2024-09-02T20:11:00Z",
+        "home_team": "Kansas City Royals",
+        "away_team": "Cleveland Guardians",
+        "bookmakers": [
+            {
+                "key": "fanduel",
+                "title": "FanDuel",
+                "last_update": "2024-09-01T19:42:12Z",
+                "markets": [
+                    {
+                        "key": "h2h",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {"name": "Cleveland Guardians", "price": -102},
+                            {"name": "Kansas City Royals", "price": -116},
+                        ],
+                    },
+                    {
+                        "key": "spreads",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {
+                                "name": "Cleveland Guardians",
+                                "price": 158,
+                                "point": -1.5,
+                            },
+                            {
+                                "name": "Kansas City Royals",
+                                "price": -192,
+                                "point": 1.5,
+                            },
+                        ],
+                    },
+                    {
+                        "key": "totals",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {"name": "Over", "price": -110, "point": 8.5},
+                            {"name": "Under", "price": -110, "point": 8.5},
+                        ],
+                    },
+                ],
+            }
+        ],
+    },
+    {
+        "id": "62728d28d14bb35560833d52819b2aab",
+        "sport_key": "baseball_mlb",
+        "sport_title": "MLB",
+        "commence_time": "2024-09-02T22:51:00Z",
+        "home_team": "Tampa Bay Rays",
+        "away_team": "Minnesota Twins",
+        "bookmakers": [
+            {
+                "key": "fanduel",
+                "title": "FanDuel",
+                "last_update": "2024-09-01T19:42:12Z",
+                "markets": [
+                    {
+                        "key": "h2h",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {"name": "Minnesota Twins", "price": -118},
+                            {"name": "Tampa Bay Rays", "price": 100},
+                        ],
+                    },
+                    {
+                        "key": "spreads",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {
+                                "name": "Minnesota Twins",
+                                "price": 142,
+                                "point": -1.5,
+                            },
+                            {"name": "Tampa Bay Rays", "price": -172, "point": 1.5},
+                        ],
+                    },
+                    {
+                        "key": "totals",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {"name": "Over", "price": -122, "point": 7.5},
+                            {"name": "Under", "price": 100, "point": 7.5},
+                        ],
+                    },
+                ],
+            }
+        ],
+    },
+    {
+        "id": "20a2d9297664412772bdee3e7c8882b5",
+        "sport_key": "baseball_mlb",
+        "sport_title": "MLB",
+        "commence_time": "2024-09-02T23:08:00Z",
+        "home_team": "Oakland Athletics",
+        "away_team": "Seattle Mariners",
+        "bookmakers": [
+            {
+                "key": "fanduel",
+                "title": "FanDuel",
+                "last_update": "2024-09-01T19:42:12Z",
+                "markets": [
+                    {
+                        "key": "h2h",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {"name": "Oakland Athletics", "price": 120},
+                            {"name": "Seattle Mariners", "price": -142},
+                        ],
+                    },
+                    {
+                        "key": "spreads",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {
+                                "name": "Oakland Athletics",
+                                "price": -140,
+                                "point": 1.5,
+                            },
+                            {
+                                "name": "Seattle Mariners",
+                                "price": 116,
+                                "point": -1.5,
+                            },
+                        ],
+                    },
+                    {
+                        "key": "totals",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {"name": "Over", "price": -118, "point": 7.5},
+                            {"name": "Under", "price": -104, "point": 7.5},
+                        ],
+                    },
+                ],
+            }
+        ],
+    },
+    {
+        "id": "a937d0250df76d52af5280fc898d5c6e",
+        "sport_key": "baseball_mlb",
+        "sport_title": "MLB",
+        "commence_time": "2024-09-02T23:11:00Z",
+        "home_team": "New York Mets",
+        "away_team": "Boston Red Sox",
+        "bookmakers": [
+            {
+                "key": "fanduel",
+                "title": "FanDuel",
+                "last_update": "2024-09-01T19:42:12Z",
+                "markets": [
+                    {
+                        "key": "h2h",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {"name": "Boston Red Sox", "price": 110},
+                            {"name": "New York Mets", "price": -130},
+                        ],
+                    },
+                    {
+                        "key": "spreads",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {"name": "Boston Red Sox", "price": -210, "point": 1.5},
+                            {"name": "New York Mets", "price": 172, "point": -1.5},
+                        ],
+                    },
+                    {
+                        "key": "totals",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {"name": "Over", "price": -108, "point": 8.0},
+                            {"name": "Under", "price": -112, "point": 8.0},
+                        ],
+                    },
+                ],
+            }
+        ],
+    },
+    {
+        "id": "fbfc5377b51a025c5ec1080c18953e37",
+        "sport_key": "baseball_mlb",
+        "sport_title": "MLB",
+        "commence_time": "2024-09-02T23:41:00Z",
+        "home_team": "Chicago Cubs",
+        "away_team": "Pittsburgh Pirates",
+        "bookmakers": [
+            {
+                "key": "fanduel",
+                "title": "FanDuel",
+                "last_update": "2024-09-01T19:42:12Z",
+                "markets": [
+                    {
+                        "key": "h2h",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {"name": "Chicago Cubs", "price": -138},
+                            {"name": "Pittsburgh Pirates", "price": 118},
+                        ],
+                    },
+                    {
+                        "key": "spreads",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {"name": "Chicago Cubs", "price": 158, "point": -1.5},
+                            {
+                                "name": "Pittsburgh Pirates",
+                                "price": -192,
+                                "point": 1.5,
+                            },
+                        ],
+                    },
+                    {
+                        "key": "totals",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {"name": "Over", "price": 100, "point": 7.5},
+                            {"name": "Under", "price": -122, "point": 7.5},
+                        ],
+                    },
+                ],
+            }
+        ],
+    },
+    {
+        "id": "fe2f59edef68a7340890a7b71c130062",
+        "sport_key": "baseball_mlb",
+        "sport_title": "MLB",
+        "commence_time": "2024-09-03T00:06:00Z",
+        "home_team": "Texas Rangers",
+        "away_team": "New York Yankees",
+        "bookmakers": [
+            {
+                "key": "fanduel",
+                "title": "FanDuel",
+                "last_update": "2024-09-01T19:42:12Z",
+                "markets": [
+                    {
+                        "key": "h2h",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {"name": "New York Yankees", "price": -200},
+                            {"name": "Texas Rangers", "price": 168},
+                        ],
+                    },
+                    {
+                        "key": "spreads",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {
+                                "name": "New York Yankees",
+                                "price": -128,
+                                "point": -1.5,
+                            },
+                            {"name": "Texas Rangers", "price": 106, "point": 1.5},
+                        ],
+                    },
+                    {
+                        "key": "totals",
+                        "last_update": "2024-09-01T19:42:12Z",
+                        "outcomes": [
+                            {"name": "Over", "price": -124, "point": 8.5},
+                            {"name": "Under", "price": 102, "point": 8.5},
+                        ],
+                    },
+                ],
+            }
+        ],
+    },
+]
