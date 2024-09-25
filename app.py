cae28ef227b36e765d98de89ffd4331ae1b578e0
Craig Phillips <cphillips@2u.com>
2024-09-24 22:23:50 -0400

    Moved past the blocker! Other Fixes
    - Blocker seemed to be resolve itself actually... something was up w/
      the API the night I was testing...
    
    - Fixed teh favicon error
    
    - Added a docker-compose file :)

--- app.py
+++ app.py
@@ -3,14 +3,23 @@
 """
 import os
 import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from core import bookie
 
 app = Flask(__name__)
 
 
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
+
+
 @app.route("/")
-# @app.route("/<sport>")
+@app.route("/<sport>")
 def get_odds(sport="mlb"):
 
     api_key = os.getenv("ODDS_API_KEY")
